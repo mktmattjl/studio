d7cea16f68f08a8a5dc6cc8c83a881afb724bd55
Norta <durando.mattei@gmail.com>
2025-05-21 10:23:43 +0000

    the hunger icon doesn't look good it needs to be a chicken thight

--- src/components/icons/PixelDrumstickIcon.tsx
+++ src/components/icons/PixelDrumstickIcon.tsx
@@ -4,17 +4,17 @@ import type { SVGProps } from 'react';
 export const PixelDrumstickIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 10 10"
+    viewBox="0 0 10 10" // Standard 10x10 viewbox
     shapeRendering="crispEdges"
     {...props}
   >
-    {/* Outline (Dark Brown) */}
-    <path fill="#784A32" d="M2,0 h1 v1 h1 v1 h2 v-1 h1 v-1 h1 v1 h1 v2 h-1 v1 h-1 v1 h-1 v1 h1 v1 h1 v1 h-1 v1 h-1 v1 h-2 v-1 H3 v-1 H2 v-1 H1 V6 h1 V5 h1 V4 h-1 V3 H2 V1 H1 V0z"/>
-    {/* Meat (Medium Brown) */}
-    <path fill="#A06B49" d="M3,1 h1 v1 h2 v1 h1 v1 h1 v1 h-1 v1 h-1 v1 h1 v1 H5 V7 H4 V6 H3 V5 H2 V2 H3z"/>
-    {/* Bone (Light Tan/Off-white) */}
-    <path fill="#F7E9DE" d="M7,0 h1 v1 H7z M2,7 h1 v1 H2z M3,8 h1 v1 H3z"/>
-    {/* Shine/Highlight on meat (Lighter Brown) */}
-    <path fill="#D3976A" d="M4,2 h1 v1 H4z M6,3 h1 v1 H6z M7,4 h1 v1 H7z"/>
+    {/* Outline (Dark Brown #784A32) */}
+    <path fill="#784A32" d="M3,0 h3 v1 h1 v1 h1 v1 h2 v1 h1 v1 h1 v1 h-1 v1 h-1 v1 h-2 v1 h-1 v1 h-1 v1 h-2 v-1 h-1 v-1 h-1 v-1 H1 v-1 H0 V2 h1 V1 h1 V0 h1z"/>
+    {/* Meat (Medium Brown #A06B49) */}
+    <path fill="#A06B49" d="M2,2 v4 h1 v1 h3 V6 h1 V2 H6 v1 H5 v1 H3 V3 H2zm4,1h1v3h-1z"/>
+    {/* Bone (Light Tan/Off-white #F7E9DE) */}
+    <path fill="#F7E9DE" d="M2,1 h2 v1 h-2z"/>
+    {/* Shine/Highlight on meat (Lighter Brown #D3976A) */}
+    <path fill="#D3976A" d="M3,5 h1 v1 h-1z"/>
   </svg>
 );
