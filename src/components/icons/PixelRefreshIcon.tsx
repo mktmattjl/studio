a0fa7898b36b5bc8ce0a817bcaae1972881211cf
Norta <durando.mattei@gmail.com>
2025-05-21 11:35:54 +0000

    but it gets error reaching server on the previe

--- src/components/icons/PixelRefreshIcon.tsx
+++ src/components/icons/PixelRefreshIcon.tsx
@@ -1,4 +1,3 @@
-
 // src/components/icons/PixelRefreshIcon.tsx
 import type { SVGProps } from 'react';
 
@@ -7,10 +6,10 @@ export const PixelRefreshIcon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
     shapeRendering="crispEdges"
-    {...props}
     fill="currentColor"
+    {...props}
   >
-    <path d="M13 3h-3v1h2v2h1V3z M3 3v3h1V4h2V3H3z M13 10h-1v2h-2v1h3v-3z M3 10v3h3v-1H4v-2H3z M6 5h1v6H6V5zm3 0h1v6H9V5z"/>
-    <path d="M8 1a7 7 0 0 0-7 7h1a6 6 0 1 1 5.21 5.9L6.5 12.5 9 15l2.5-2.5L10.79 11.1A6 6 0 0 1 8 13a6 6 0 0 1-6-6H1a7 7 0 0 0 14 0h-1a6 6 0 0 1-6 5z"/>
+    {/* Simple square for testing server stability */}
+    <path d="M6 6h4v4H6z" />
   </svg>
 );
