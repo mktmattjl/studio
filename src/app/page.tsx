783471c32137d70ef4570cdcfa52b5817bd67029
Norta <durando.mattei@gmail.com>
2025-08-13 09:43:49 +0000

    perfect it's good now re organise the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,10 +45,10 @@ export default function DashboardPage() {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2 space-y-6">
-                <TaskList />
+                <DashboardPlannerPreview />
             </div>
             <div className="space-y-6">
-                <DashboardPlannerPreview />
+                <TaskList />
                 <StudySpinner topics={studyTopics} />
             </div>
         </div>
